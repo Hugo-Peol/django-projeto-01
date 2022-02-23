8e0abf9781016075b805c47965f403b1706fe72d
Hugo Pereira Oliveira <hugo.pe.po@hotmail.com>
2022-02-23 19:00:37 -0300

    ajustes, estilo CSS da header, criando partial

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,7 @@
-
 from django.urls import path
 
 from recipes.views import home
 
 urlpatterns = [
-path('', home)
+    path('', home),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
 
-# from django.shortcuts import render
-
 
 def home(request):
-    return render(request, 'recipes/home.html')
-
-
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Luiz Otávio',
+    })
