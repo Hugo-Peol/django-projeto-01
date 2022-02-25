84a89c980dd10b5c8374a58acc61f0a99cc4c2f8
Hugo Pereira Oliveira <hugo.pe.po@hotmail.com>
2022-02-25 02:32:35 -0300

    name space errado?

--- projeto/settings.py
+++ projeto/settings.py
@@ -119,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_FILES = [
+    BASE_DIR / 'base_statick',
+]
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
